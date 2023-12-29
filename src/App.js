98c166a6d1a44eb566f488b3597709354f1a0e95
VladVoV <vladojch@gmail.com>
2023-12-30 01:28:26 +0200

    Added login and register handling, also added different boards for user, admin and moderator

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import ReactDOM from "react-dom/client";
 import MainPage from "./Pages/Main page";
 import ToDoListPage from "./Pages/To-Do List Page";
 import Calendar from "./Pages/Calendar";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
+import Home from "./Pages/Main page";
+import Profile from "./Components/Profile";
+import BoardUser from "./Pages/BoardUser";
+import BoardModerator from "./Pages/BoardModerator";
+import BoardAdmin from "./Pages/BoardAdmin";
 
 function App() {
   return (
@@ -11,9 +18,14 @@ function App() {
           <Route path={'/'} element={<MainPage/>}/>
           <Route path={'/to-do'} element={<ToDoListPage/>}/>
           <Route path={'calendar'} element={<Calendar/>}/>
+          <Route path={"/home"} element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/user" element={<BoardUser/>}/>
+          <Route path="/mod" element={<BoardModerator/>}/>
+          <Route path="/admin" element={<BoardAdmin/>}/>
       </Routes>
         );
 }
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>);
 export default App;
