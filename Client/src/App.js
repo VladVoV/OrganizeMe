40da4fc9591fbd62104762d67b28c9992588726e
VladVoV <vladojch@gmail.com>
2024-01-25 13:46:30 +0200

    Updated login and register to use react-hook-forms

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {Routes, Route } from "react-router-dom";
-import ReactDOM from "react-dom/client";
 import MainPage from "./Pages/Main page";
 import ToDoListPage from "./Pages/To-Do List Page";
 import Calendar from "./Pages/Calendar";
